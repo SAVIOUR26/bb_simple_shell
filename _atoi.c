69a75ee4b80af373ebae785039da4daf974541af
SAVIOUR NAJUNA <saviournajuna@gmail.com>
2023-07-29 14:05:38 +0300

    Update _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -1,64 +1,74 @@
 #include "shell.h"
+
 /**
-* interactive - this returns true if the shell is in interactive mode
-* @info: structure address
-**
-Return: 1 if in the interactive mode, else 0
-*/
+ * interactive - this returns true if the shell is in interactive mode
+ * @info: the structure address
+ *
+ * Return: 1 if in the interactive mode, else 0
+ */
 int interactive(info_t *info)
 {
-return (isatty(STDIN_FILENO) && info->readfd <= 2);
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 /**
-* is_delim - this checks if the character is a delimiter
-* @c: char to check
-* @delim: the delimiter string
-* Return: 1 if true, 0 if false
-*/
+ * is_delim - this checks if the character is a delimiter
+ * @c: char to check
+ * @delim: delimiter string
+ * Return: 1 if true, 0 if false
+ */
 int is_delim(char c, char *delim)
 {
-while (*delim)
-if (*delim++ == c)
-return (1);
-return (0);
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
 }
+
 /**
-* _isalpha - checks for alphabetic character
-* @c: The character to input
-* Return: 1 if c is alphabetic, 0 otherwise
-*/
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
+ */
+
 int _isalpha(int c)
 {
-if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-return (1);
-else
-return (0);
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
 }
+
 /**
-* _atoi - this converts the string to an integer
-* @s: string to be converted
-* Return: 0 if no numbers in the string, converted number otherwise
-*/
+ *_atoi - this converts the string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in the string, else converted number
+ */
+
 int _atoi(char *s)
 {
-int i, sign = 1, flag = 0, output;
-unsigned int result = 0;
-for (i = 0; s[i] != '\0' && flag != 2; i++)
-{
-if (s[i] == '-')
-sign *= -1;
-if (s[i] >= '0' && s[i] <= '9')
-{
-flag = 1;
-result *= 10;
-result += (s[i] - '0');
-}
-else if (flag == 1)
-flag = 2;
-}
-if (sign == -1)
-output = -result;
-else
-output = result;
-return (output);
+	int i, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (i = 0;  s[i] != '\0' && flag != 2; i++)
+	{
+		if (s[i] == '-')
+			sign *= -1;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[i] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
 }
